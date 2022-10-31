3eb5ddb4d6c05e0c20421b6d5bafedf93967ed26
harry <zh_harry@163.com>
2022-10-31 13:31:09 +0800

    init security

--- security-po/src/main/java/com/sparrow/security/po/App.java
+++ security-po/src/main/java/com/sparrow/security/po/App.java
@@ -11,4 +11,36 @@ public class App extends PO {
     private String code;
     private String name;
     private Integer sort;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
 }
--- server/security-main-spring-boot/src/test/java/com/sparrow/spring/reflect/ThreeDesTest.java
+++
@@ -1,9 +0,0 @@
-package com.sparrow.spring.reflect;
-
-import com.sparrow.cryptogram.ThreeDES;
-
-public class ThreeDesTest {
-    public static void main(String[] args) {
-       String threeDes=ThreeDES.getInstance().encrypt("ssss", "sssss");
-    }
-}
